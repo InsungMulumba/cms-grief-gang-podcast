1557cb6c12d915f4a157b0e035881c5a71d8095e
Insung <insungmul@live.co.uk>
2021-11-04 11:30:43 +0000

    blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,6 +3,8 @@ import MainLayout from "../layouts/mainLayout";
 import styled from "styled-components";
 import colors from "../styles/colors";
 import PageWithLayoutType from "../types/pageWithLayout";
+import Header from "../components/Header/Header";
+import Head from "next/head";
 
 const Root = styled.div`
     display: flex;
@@ -16,9 +18,21 @@ const Root = styled.div`
 
 const Blog: FC = () => {
   return (
-    <Root>
-      <h1>Blog Page Coming Soon!</h1>
-    </Root>
+    <><Head>
+      <title>The Grief Gang | Podcast</title>
+      <link rel="icon" href="/logo.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;700&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Head><Header isMain={true} showBulletin={false} /><Root>
+        <h1>Blog Page Coming Soon!</h1>
+      </Root></>
   );
 };
 
