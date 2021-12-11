40cee07ac9e893b877d56c1760ce1e5b1abb1b5c
Insung <insungmul@live.co.uk>
2021-12-11 20:14:30 +0000

    new home page

--- components/Home/Banner.tsx
+++ components/Home/Banner.tsx
@@ -1,7 +1,7 @@
 import React, { Component, FC } from "react";
 import styled, { keyframes } from "styled-components";
 import colors from "styles/colors";
-import {isSafari, isMobileSafari } from "react-device-detect";
+import { isSafari, isMobileSafari } from "react-device-detect";
 
 const OverlaySlideIn = keyframes`
     0% {
@@ -17,11 +17,9 @@ const OverlaySlideIn = keyframes`
 const SlideInLogo = keyframes`
     0% {
       left: -40%;
-      /* opacity: 0%; */
     }
     100% {
       left: 20;
-      /* opacity: 100%; */
     }
 `;
 
@@ -39,7 +37,7 @@ const Bounce = keyframes`
  }
 `;
 
-const SlideInArrow = keyframes`
+const FadeIn = keyframes`
     0% {
       opacity: 0%;
     }
@@ -47,22 +45,18 @@ const SlideInArrow = keyframes`
       opacity: 100%;
     }
 `;
+
 const BannerOverlay = styled.a`
   width: 100%;
   height: 100vh;
-  background-color: ${colors.mainPink};
+  /* background-color: ${colors.mainPink}; */
   position: absolute;
   opacity: 0.5;
   animation: ${OverlaySlideIn} 1.5s ease-out 1.5s both;
 `;
 
-const safari = isSafari || isMobileSafari;
-
-console.log(`isSafari: ${safari}`);
-// const imageURL = safari ? "/Home/amber-main.jpg" : "/Home/amber-main-webp.webp";
-// const bAttachment = safari ? 'scroll' : 'fixed';
 const BannerImageContainer = styled.div`
-  background-image: url("/Home/amber-main.jpg");
+  /* background-image: url("/Home/amber-main.jpg"); */
 
   /* Set a specific height */
   height: 100vh;
@@ -71,33 +65,15 @@ const BannerImageContainer = styled.div`
 
   background-position: center;
   background-repeat: no-repeat;
-  background-size: cover; 
-
+  background-size: cover;
 
   /* Chrome version 29 and above */
-@media screen and (-webkit-min-device-pixel-ratio:0)
-and (min-resolution:.001dpcm) {
-selector{ 
-  
-    /* Create the parallax scrolling effect */
-    background-attachment: "fixed";
-
-
-}
-}
-
-  /* position: relative; */
-
-  /* @media not all and (min-resolution: 0.001dpcm) {
-    @supports (-webkit-appearance: none) {
-      .safari_only {
-        background-attachment: scroll;
-        background-image: url("/Home/amber-main.jpg");
-
-      }
+  @media screen and (-webkit-min-device-pixel-ratio: 0) and (min-resolution: 0.001dpcm) {
+    selector {
+      /* Create the parallax scrolling effect */
+      background-attachment: "fixed";
     }
-  } */
-
+  }
 `;
 
 const Logo = styled.img`
@@ -105,25 +81,28 @@ const Logo = styled.img`
   top: 20%;
   height: 200px;
   width: 200px;
-  left: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
   animation: ${SlideInLogo} 1.5s ease-out 2.5s both;
   @media (max-width: 1279px) {
-    height: 250px;
-    width: 250px;
+    height: auto;
+    width: 100vw;
+    padding: 0px 60px;
     left: 50%;
-    top: 50%;
+    top: 60%;
     transform: translate(-50%, -50%);
   }
 `;
 
 const ArrowDown = styled.img`
-  height: 45px;
-  width: 45px;
+  height: 65px;
+  width: 65px;
   animation: ${Bounce} 1.5s linear infinite;
 `;
 
 const ArrowDownContainer = styled.div`
-  animation: ${SlideInArrow} 1.5s ease-out 2.5s both;
+  animation: ${FadeIn} 1.5s ease-out 2.5s both;
 
   position: absolute;
   left: 0%;
@@ -131,6 +110,28 @@ const ArrowDownContainer = styled.div`
   display: flex;
   bottom: 5%;
   justify-content: center;
+`;
+
+const Slogan = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: ${colors.secondary};
+  font-size: 64px;
+  font-weight: 900;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  animation: ${FadeIn} 1.5s ease-out 0.5s both;
+
+  @media (max-width: 1279px) {
+    top: 5%;
+    left: 0%;
+    transform: none;
+    font-size: 32px;
+    padding: 0px 60px;
+  }
 `;
 
 function scrollToHome(e) {
@@ -142,7 +143,7 @@ function scrollToHome(e) {
 const Banner: FC = () => {
   return (
     <>
-      <BannerImageContainer className='banner' id='banner' />
+      <BannerImageContainer className="banner" id="banner" />
       <BannerOverlay href="/" onClick={scrollToHome} />
       <Logo
         onClick={scrollToHome}
@@ -150,10 +151,13 @@ const Banner: FC = () => {
         alt="Grief Gang Podcast logo"
         crossOrigin="anonymous"
       />
+      <Slogan>
+        Welcome to the gang. <br /> The one you never asked to be a part of.
+      </Slogan>
       <ArrowDownContainer>
         <ArrowDown
           onClick={scrollToHome}
-          src="/Home/down-arrow.png"
+          src="/Home/down-arrow-black.png"
           alt="Scroll Down Button"
           crossOrigin="anonymous"
         />
